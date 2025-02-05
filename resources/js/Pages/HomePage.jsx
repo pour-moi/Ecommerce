f92a1ce16b8e7135f6cae87cace83688645820bd
pour-moi <pour64moi@gmail.com>
2025-02-05 04:37:46 +0300

    receives product by category

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -3,13 +3,25 @@ import Hero from "@/Layouts/Hero";
 import Category from "@/Components/Category";
 import Products from "@/Components/Products";
 import Footer from "@/Layouts/Footer";
-export default function HomePage({ product }) {
+export default function HomePage({
+    electronics_product,
+    groceries_product,
+    furniture_product,
+    cosmotics_product,
+}) {
     return (
         <>
             <NavBar />
             <Hero />
             <Category />
-            <Products products={product} />
+            <Products
+                products={[
+                    electronics_product,
+                    groceries_product,
+                    furniture_product,
+                    cosmotics_product,
+                ]}
+            />
             <Footer />
         </>
     );
